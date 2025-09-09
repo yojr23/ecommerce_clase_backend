5cca941e14f9f5ae6a893bf7afff2556eb553fad
yojr23 <juniorrincon1992@gmail.com>
2025-09-09 14:31:18 -0500

    grupo de rutas: products

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,9 @@ use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/products',[ProductController::class, 'index']);
 
-
-Route::get('/products/create',[ProductController::class, 'create']);
-
-Route::get('/products/{id}/{category?}',[ProductController::class, 'detail']);
-
+Route::prefix('products')->controller(ProductController::class)->group(function() {
+    Route::get('/', ['index']);
+    Route::get('/create', ['create']);
+    Route::get('/{id}/{category?}', ['detail']);
+});
