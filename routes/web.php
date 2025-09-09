70fa035e3e4031d7cc6b452b4e4d4a5a80d883b6
yojr23 <juniorrincon1992@gmail.com>
2025-09-09 13:51:22 -0500

    rutas basicas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    return "List products";
+});
+
+Route::get('/products/{id}/{category?}', function ($name,$category = null) {
+    if ($category != null) {
+        return "Detail products" . $name . " With category: " . $category;
+    } else{
+        return "Detail products" . $name;
+    }
+});
