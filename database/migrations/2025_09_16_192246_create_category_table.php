79b9fbbe7927abd20cb506f1dd3df3b92fbc0c33
yojr23 <juniorrincon1992@gmail.com>
2025-09-16 14:25:17 -0500

    category table

--- database/migrations/2025_09_16_192246_create_category_table.php
+++ database/migrations/2025_09_16_192246_create_category_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product', function (Blueprint $table) {
+        Schema::create('category', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('product');
+        Schema::dropIfExists('category');
     }
 };
